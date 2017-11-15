184e2e1b1751642f26456ff6c2fed097e29e9a69
antoni <antoni@csltd.com.ua>
2017-11-15 15:44:04 +0200

    новый класс HW

--- src/test/java/MaxNumber.java
+++ src/test/java/MaxNumber.java
@@ -1,14 +1,17 @@
 /**
  * Created by antoni on 14.11.2017.
  */
+
+
 public class MaxNumber {
+
 
     public static  void main (String [] args) {
 
         int a = 10;
         int g = 20;
-        System.out.println(a);
-        System.out.println(g);
+        System.out.println("Большее число из 2 чисел: " + Math.max(a,g));
+        System.out.println("Меньшее число из 2 чисел: " + Math.min(a,g));
     }
 
 
