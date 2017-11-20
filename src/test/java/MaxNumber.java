41adeebb79f5f0d01a018133844bc50627da38c5
antoni <antoni@csltd.com.ua>
2017-11-20 16:04:35 +0200

    новый класс HW

--- src/test/java/MaxNumber.java
+++ src/test/java/MaxNumber.java
@@ -1,18 +1,34 @@
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by antoni on 14.11.2017.
  */
 
 
+
 public class MaxNumber {
 
+  //  @Title("Создание уполномоченного лица для удаления/редактирования")
 
+    @Test
     public static  void main (String [] args) {
 
         int a = 10;
         int g = 20;
-        System.out.println("Большее число из 2 чисел: " + Math.max(a,g));
-        System.out.println("Меньшее число из 2 чисел: " + Math.min(a,g));
+        MaxNumber c = new MaxNumber();
+       System.out.println("Большее число из 2 чисел: " + Math.max(a,g));
+       System.out.println("Меньшее число из 2 чисел: " + Math.min(a,g));
+
+       // assertEquals("Unexpected string value", Math.max(a,g));
+
+
     }
 
 
+
 }
+
+
