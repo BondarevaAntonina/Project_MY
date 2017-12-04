615bc15cd95191cff1fc3057ed575dca193fbab0
antoni <antoni@csltd.com.ua>
2017-12-04 17:25:08 +0200

    новый класс HW

--- src/main/java/WorkNumber.java
+++ src/main/java/WorkNumber.java
@@ -3,23 +3,18 @@
  */
 public class WorkNumber {
 
-    public int a; // 1-е число
-    public int g; // 2-е число
+  /*  public int a=10; // 1-е число
+    public int g=25; // 2-е число*/
 
 
-    public int getMaxNumber(int i1, int i2) {
+    public static int getMaxNumber(int i1, int i2) {
         if (i1 < 0 || i2 < 0) {
             return 0;
         }
         return Math.max(i1, i2);
     }
 
-    public int getMinNumber(int i1, int i2) {
-        if (i1 > 0 || i2 > 0) {
-            return 0;
-        }
-        return Math.max(i1, i2);
-    }
+
 }
 
 
--- src/test/java/MaxNumber.java
+++ src/test/java/MaxNumber.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 import ru.yandex.qatools.allure.annotations.Title;
 
@@ -9,10 +10,21 @@ import ru.yandex.qatools.allure.annotations.Title;
 public class MaxNumber {
   //  private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    @Title("Создание нового валютного документа c подписями  для удаления ")
+    @Title("i1=10 и i1=20 ")
     @Test
     public void testMaxNumber01 () throws Exception {
+        Assert.assertEquals(20,WorkNumber.getMaxNumber(10,20));
+    }
 
+    @Title("i1=-10 и i1=-20 ")
+    @Test
+    public void testMaxNumber02 () throws Exception {
+        Assert.assertEquals(-10,WorkNumber.getMaxNumber(-10,-20));
+    }
 
+    @Title("i1=-10 и i1=-20 ")
+    @Test
+    public void testMaxNumber03 () throws Exception {
+        Assert.assertEquals(-10,WorkNumber.getMaxNumber(-10,-20));
     }
 }
