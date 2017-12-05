1b51efd7f92912e609bace73fa74b02b655fb2b1
antoni <antoni@csltd.com.ua>
2017-12-05 16:51:11 +0200

    тестовое задание 2

---
+++ src/main/java/PageObject/HomePage.java
@@ -0,0 +1,16 @@
+package PageObject;
+
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by antoni on 05.12.2017.
+ */
+public class HomePage {
+    private EventFiringWebDriver driver;
+    private static final Logger LOGGER =  LoggerFactory.getLogger(HomePage.class);
+
+
+
+}
