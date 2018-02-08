b546f3f1e4011800a67979e57aa31c52a894892e
antoni <antoni@csltd.com.ua>
2018-02-08 11:22:25 +0200

    Описание эллементов

--- src/main/java/PageObject/HomePage.java
+++ src/main/java/PageObject/HomePage.java
@@ -2,6 +2,7 @@ package PageObject;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,24 +10,36 @@ import org.slf4j.LoggerFactory;
 /**
  * Created by antoni on 05.12.2017.
  */
-public class HomePage extends AbstractPageObject {
+
+public class HomePage {
     private EventFiringWebDriver driver;
     private static final Logger LOGGER =  LoggerFactory.getLogger(HomePage.class);
 
 
 
     @FindBy(name =".//a[@data-title='Смартфоны, ТВ и электроника'] " )
-    private WebElement smartElectronic;
+    private WebElement SmartElectronic;
 
     @FindBy(xpath =".//a[contains(.,'Портативная электроника')] " )
-    private WebElement portElectronic;
+    private WebElement PortElectronic;
 
     @FindBy(xpath =".//a[contains(.,'Наушники')] " )
-    private WebElement naushnici;
+    private WebElement Naushnici;
 
     public HomePage(EventFiringWebDriver driver) {
-        super(driver);
+            PageFactory.initElements(driver, this);
+            this.driver = driver;
     }
 
 
+    public  void goMenuClick (EventFiringWebDriver driver) throws  Exception {
+         SmartElectronic.click();
+    }
+
+
+
 }
+
+
+
+
