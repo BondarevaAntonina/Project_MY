b234264d8098e281042b598d6032c1f984598776
antoni <antoni@csltd.com.ua>
2018-03-15 12:38:58 +0200

    многомерные массивы

--- src/main/java/lesson/Multidimensional_arrays.java
+++ src/main/java/lesson/Multidimensional_arrays.java
@@ -28,7 +28,11 @@ public class Multidimensional_arrays {
         int[][] matrice = {{1, 2, 3},
                 {4, 5, 6},
                 {7, 8, 9}};
-        System.out.println(matrice[2][2]);
-
+        for (int i = 0; i < matrice.length; i++) {
+            for (int j = 0; j < matrice.length; j++) {
+                System.out.print(matrice[i][j] + "");
+            }
+            System.out.println();
+        }
     }
 }
