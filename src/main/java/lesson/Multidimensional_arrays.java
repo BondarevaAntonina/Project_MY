ab1cf788364f5dbbaa4ef14d1ec1d9bb401471b1
antoni <antoni@csltd.com.ua>
2018-03-15 10:02:58 +0200

    Switch

--- src/main/java/lesson/Multidimensional_arrays.java
+++ src/main/java/lesson/Multidimensional_arrays.java
@@ -3,8 +3,32 @@ package lesson;
 /**
  * Created by antoni on 15.03.2018.
  */
+//public class Multidimensional_arrays {
+//    public static void main(String[] args) {
+//        int[] number = {1, 2, 3};
+//        System.out.println(number[1]);
+//        int[][] matrice = {{1, 2, 3, 4, 5},
+//                             {4, 5, 6},
+//                             {7, 8, 9}};
+//        System.out.println(matrice[2][2]);
+//        System.out.println(matrice[1][0]);
+//        System.out.println(matrice[0][4]);
+//
+//        int[] nambers1 = new int[5];
+//        String[][] strings = new String[2][3];
+//        strings[0][1] = "Привет";
+//        System.out.println(strings[0][1]);
+//    }
+//}
+
+
 public class Multidimensional_arrays {
     public static void main(String[] args) {
+        int[] number = {1, 2, 3};
+        int[][] matrice = {{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+        System.out.println(matrice[2][2]);
 
     }
 }
