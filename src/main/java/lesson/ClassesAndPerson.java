eb5afa7f91caae6abbf27381f35c01de2ef30528
antoni <antoni@csltd.com.ua>
2018-03-19 18:05:09 +0200

    методы параметры метода

--- src/main/java/lesson/ClassesAndPerson.java
+++ src/main/java/lesson/ClassesAndPerson.java
@@ -6,15 +6,16 @@ package lesson;
 public class ClassesAndPerson {
     public static void main(String[] args) {
         PersonPeaple person1 = new PersonPeaple();
-        person1.name = "Рома";
-        person1.age = 50;
+        person1.setNameAndAge("Рома", 50);// person1.name = "Рома";
+        String s1= "Вова";
         PersonPeaple person2 = new PersonPeaple();
-        person2.name = "Вова";
-        person2.age = 20;
-        int years1 = person1.calculateYearsToRetirement();
-        int years2 =person2.calculateYearsToRetirement();
-        System.out.println("Первому человеку до пенсии: "+years1+" лет");
-        System.out.println("Второму человеку до пенсии: "+years2+" лет");
+        person2.setNameAndAge(s1,30);//person2.name = "Вова";
+        person1.speak();
+        person2.speak();
+//        int years1 = person1.calculateYearsToRetirement();
+//        int years2 = person2.calculateYearsToRetirement();
+//        System.out.println("Первому человеку до пенсии: " + years1 + " лет");
+//        System.out.println("Второму человеку до пенсии: " + years2 + " лет");
     }
 }
 
@@ -22,7 +23,12 @@ class PersonPeaple {
     String name;
     int age;
 
-    int calculateYearsToRetirement(){
+    void setNameAndAge(String username, int userage) {
+        name = username;
+        age = userage;
+    }
+
+    int calculateYearsToRetirement() {
         int years = 65 - age;
         return years;
     }
@@ -34,7 +40,7 @@ class PersonPeaple {
 
     void speak() {
         for (int i = 0; i < 3; i++) {
-            System.out.println("Меня зовут " + name + ",мне" + age + " лет");
+            System.out.println("Меня зовут " + name + ",мне " + age + " лет");
         }
     }
 
