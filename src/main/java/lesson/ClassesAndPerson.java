0f30264ee03593d13e7e31883ac141f13c848277
antoni <antoni@csltd.com.ua>
2018-03-19 13:07:13 +0200

    методы return

--- src/main/java/lesson/ClassesAndPerson.java
+++ src/main/java/lesson/ClassesAndPerson.java
@@ -8,18 +8,29 @@ public class ClassesAndPerson {
         PersonPeaple person1 = new PersonPeaple();
         person1.name = "Рома";
         person1.age = 50;
-        person1.speak();
         PersonPeaple person2 = new PersonPeaple();
         person2.name = "Вова";
         person2.age = 20;
-        person2.sayHello();
-
+        int years1 = person1.calculateYearsToRetirement();
+        int years2 =person2.calculateYearsToRetirement();
+        System.out.println("Первому человеку до пенсии: "+years1+" лет");
+        System.out.println("Второму человеку до пенсии: "+years2+" лет");
     }
 }
 
 class PersonPeaple {
     String name;
     int age;
+
+    int calculateYearsToRetirement(){
+        int years = 65 - age;
+        return years;
+    }
+
+//    void calculateYearsToRetirement() {
+//        int years = 65 - age;
+//        System.out.println("Количество лет до пенсии "+years);
+//    }
 
     void speak() {
         for (int i = 0; i < 3; i++) {
