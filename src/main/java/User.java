924902320174ca44a84d04ee667753b8be58e815
antoni <antoni@csltd.com.ua>
2018-03-21 18:17:24 +0200

    Static

---
+++ src/main/java/User.java
@@ -0,0 +1,32 @@
+/**
+ * Created by antoni on 21.03.2018.
+ */
+public class User {
+    private static int minAgeToWorkInOurCompany;
+    private String name;
+    private static  String lastName;
+
+    public static int getMinAgeToWorkInOurCompany() {
+        return minAgeToWorkInOurCompany;
+    }
+
+    public static void setMinAgeToWorkInOurCompany(int minAgeToWorkInOurCompany) {
+        User.minAgeToWorkInOurCompany = minAgeToWorkInOurCompany;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
