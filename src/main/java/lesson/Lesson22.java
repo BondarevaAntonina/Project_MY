3eacbaf8ef233854fb592870b2d8f71e92c0e1df
antoni <antoni@csltd.com.ua>
2018-03-22 17:27:25 +0200

    Ключевое слово final

--- src/main/java/lesson/Lesson22.java
+++ src/main/java/lesson/Lesson22.java
@@ -5,6 +5,14 @@ package lesson;
  */
 public class Lesson22 {
     public static void main(String[] args) {
+        final int X = 10;
+        System.out.println(X);
 
     }
+}
+
+class Test {
+    public static final int CONSTANT = 0;
+
+
 }
