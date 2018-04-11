5da8a682cd58d3b2b3fa8545929ee7f16a589d77
antoni <antoni@csltd.com.ua>
2018-04-11 10:23:06 +0300

    массив

---
+++ src/main/java/Valevskiy/IntArrayList.java
@@ -0,0 +1,13 @@
+package Valevskiy;
+
+/**
+ * Created by antoni on 10.04.2018.
+ */
+public class IntArrayList {
+    public static void main(String[] args) {
+        int[] myArray = new int[27];
+
+    }
+
+}
+
