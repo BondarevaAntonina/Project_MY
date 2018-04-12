c813f5121dc6bf93ce6d2add01e2e19db89e3e3e
antoni <antoni@csltd.com.ua>
2018-04-12 10:03:11 +0300

    массив

--- src/main/java/Valevskiy/IntArrayList.java
+++ src/main/java/Valevskiy/IntArrayList.java
@@ -1,5 +1,8 @@
 package Valevskiy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by antoni on 10.04.2018.
  */
@@ -8,6 +11,8 @@ public class IntArrayList {
         int[] myArray = new int[27];
         for (int i = 0; i < 27; i++) {
             myArray[i] = i;
+//            List<Integer> list = new ArrayList<Integer>();
+//            list.add(27);
             System.out.println("Последний  элемент массива " + myArray[i]);
         }
         int razmer = myArray.length;
--- src/main/java/Valevskiy/ManagerIntArrayList.java
+++ src/main/java/Valevskiy/ManagerIntArrayList.java
@@ -16,9 +16,14 @@ public class ManagerIntArrayList {
             System.out.println("------- Меню -----------");
             System.out.println("1 - Печать всех элементов");
             System.out.println("2 - Добавлеие элемента в список");
-            System.out.println("2 - Изъятие элемента из списока");
-            System.out.println("3 - Расширение списка");
-            System.out.println("4 - Уменьшение списка");
+            System.out.println("3 - Изменение/удаление элементов по индексу");
+            System.out.println("4 - Увеличение листа на заданное количество элементов");
+            System.out.println("5 - Уменьшение листа до заданного количество элементов");
+            System.out.println("6 - Вывод элементов в консоль в прямом и обратном порядке");
+            System.out.println("7 - сортировка листа методом пузырька");
+            System.out.println("8 - добавление массива в массив");
+            System.out.println("9 - Удалять дубликаты");
+            System.out.println("3 - Поиск элемента методом линейного поиска");
             System.out.println("0 - Выход");
             try{
                 actionNumber = Integer.parseInt(reader.readLine());
@@ -28,6 +33,7 @@ public class ManagerIntArrayList {
             }
         }while(actionNumber != 0);
     }
+
 
 }
 
