2a0aef61650071bf8d0d53caa06481f95ff1ca6d
antoni <antoni@csltd.com.ua>
2018-04-17 13:29:58 +0300

    массив

--- src/main/java/lessons/IncrementDecrement/MassEvenNumbers.java
+++ src/main/java/lessons/IncrementDecrement/MassEvenNumbers.java
@@ -14,7 +14,6 @@ public class MassEvenNumbers {
             if (i % 2 == 0) a++;
         }
         int[] mas1 = new int[a];
-        int[] mas2 = new int[a];
         for (int i = 2, b = 0; i <= 20; i++) {
             if (i % 2 == 0) {
                 mas1[b] = i;
@@ -22,21 +21,17 @@ public class MassEvenNumbers {
                 b++;
             }
         }
-
+        System.out.println("");
         //Проинициализировать второй массив элементами первого массива при условии, что индекс делится на 4 без остатка и элемент больше 3, но меньше 16
 
-        int n = 0;
-        for (int x = 0; x < mas2.length; x++) {
-            if (mas2[x] % 2 - 1 == 0) {
-                n++;
-                a += mas2[x];
-
-                System.out.println("Нечетный элемент = " + mas2[a]);
+        int[] mas2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        int i = 0;
+        for (int myInt : mas2) {
+            if (myInt % 4 == 0) {
+                if (myInt > 3 && myInt <= 16) {
+                    System.out.println("Значение элемента равно:" + myInt++);
+                }
             }
         }
     }
 }
-
-
-//        System.out.println( "Всего нечетных элементов = " + n );
-//        System.out.println( "Cумма нечетных элементов = " + sum );
