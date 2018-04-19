5d7540e877181de18963b207446056a41c4b5b01
antoni <antoni@csltd.com.ua>
2018-04-19 18:43:44 +0300

    первое задание

--- src/main/java/homeworks/one_dim_array/ArrayTwoDimensional.java
+++ src/main/java/homeworks/one_dim_array/ArrayTwoDimensional.java
@@ -5,24 +5,28 @@ package homeworks.one_dim_array;
  * В двумерном массиве Вывести максимальный элемент каждой строки
  */
 public class ArrayTwoDimensional {
-    public static void main(String[] args){
-        int arrayCount = 8;
+    public static void main(String[] args) {
+        int arrayCount = 4;
         int[][] massive = new int[arrayCount][arrayCount];
-        for(int i=0;i<arrayCount;i++){
-            for(int j=0;j<arrayCount;j++){
-                massive[i][j] = ((int)Math.round(Math.random()*100));
-                System.out.println(massive[i][j]+ " ");
-            }
-        }
-        int max = massive[0][0];
-        for(int i=0; i<arrayCount-1; i++){
-            for(int j=0; j<arrayCount; j++){
-                if(massive[i][j]<max){
-                    max = massive[i][j];
+        for (int i = 0; i < arrayCount; i++) {
+            for (int j = 0; j < arrayCount; j++) {
+                massive[i][j] = ((int) Math.round(Math.random() * 20));
+                System.out.print(massive[i][j] + "\t");
+                int max = massive[0][0];
+                for (int d = 0; d < arrayCount - 1; d++) {
+                    for (int b = 0; b < arrayCount; b++) {
+                        if (massive[i][j] < max) {
+                            max = massive[i][j];
+                        }
+                    }
+                    System.out.print(max+"\t");
                 }
             }
-        }
+            System.out.println();
 
-        System.out.print(max);
+
+
+        }
     }
 }
+
