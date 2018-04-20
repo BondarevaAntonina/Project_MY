1ff8df7aab85dc8f27ee52596760360e29a9165d
antoni <antoni@csltd.com.ua>
2018-04-20 17:22:38 +0300

    первое задание

--- src/main/java/homeworks/one_dim_array/BankAccount.java
+++ src/main/java/homeworks/one_dim_array/BankAccount.java
@@ -1,5 +1,7 @@
 package homeworks.one_dim_array;
 
+import org.eclipse.jetty.util.IO;
+
 import java.util.Scanner;
 
 /**
@@ -8,5 +10,39 @@ import java.util.Scanner;
  * сумму. Если денег недостаточно,  тогда написать в консоль - "Пополните счет".  Счет можно пополнить другим методом.
  */
 public class BankAccount {
+    private double balance = 200.0;
+    private String owner = "Владелец";
+    private String currency;
+
+
+    public BankAccount() {
+    }
+
+    public BankAccount(double initialBalance, String name, String currencyDollar) {
+        balance = initialBalance;
+        owner = name;
+        currencyDollar = currency;
+
+    }
+
+    public String getValuta() {
+        return currency;
+    }
+
+    public void setValuta(String valuta) {
+        this.currency = valuta;
+    }
+
+    public double getSum() {
+        return balance;
+    }
+
+    public void setSum(double sum) {
+        this.balance = sum;
+    }
+
 
 }
+
+
+
