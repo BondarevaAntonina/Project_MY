f2e8201a07e66038e40c303928706ba0c0c5ffb1
antoni <antoni@csltd.com.ua>
2018-04-20 13:10:42 +0300

    первое задание

--- src/main/java/homeworks/one_dim_array/ArrayTwoDimensional.java
+++ src/main/java/homeworks/one_dim_array/ArrayTwoDimensional.java
@@ -6,27 +6,20 @@ package homeworks.one_dim_array;
  */
 public class ArrayTwoDimensional {
     public static void main(String[] args) {
+
         int arrayCount = 4;
+
         int[][] massive = new int[arrayCount][arrayCount];
+
         for (int i = 0; i < arrayCount; i++) {
+
             for (int j = 0; j < arrayCount; j++) {
-                massive[i][j] = ((int) Math.round(Math.random() * 20));
+
+                massive[i][j] = ((int) Math.round(Math.random() * 9));
+
                 System.out.print(massive[i][j] + "\t");
-                int max = massive[0][0];
-                for (int d = 0; d < arrayCount - 1; d++) {
-                    for (int b = 0; b < arrayCount; b++) {
-                        if (massive[i][j] < max) {
-                            max = massive[i][j];
-                        }
-                    }
-                    System.out.print(max+"\t");
-                }
             }
             System.out.println();
-
-
-
         }
     }
 }
-
