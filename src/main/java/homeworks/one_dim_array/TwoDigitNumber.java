a40d9e55948f5aff198c9769b0cce9546d1049b5
antoni <antoni@csltd.com.ua>
2018-04-25 09:04:45 +0300

    калюкулятор

--- src/main/java/homeworks/one_dim_array/TwoDigitNumber.java
+++ src/main/java/homeworks/one_dim_array/TwoDigitNumber.java
@@ -14,30 +14,24 @@ public class TwoDigitNumber {
         Scanner in = new Scanner(System.in);
 
             int row = 4;
-
             int column = 4;
-
             Random random = new Random();
-
             int[][] array = new int[row][column];
-
             for (int x = 0; x < row; x++) {
                 for (int j = 0; j < column; j++) {
                     array[x][j] = random.nextInt(100);
                     System.out.print(array[x][j] + "\t");
                 }
                 System.out.println();
-
             }
+
             int sum = 0;
         while (true) {
+//            int number = in.nextInt();
             int number = in.nextInt();
             if (number >= 100 || number <= 9) {
                 break;
             } else {
-                String str = "" + number;
-                String numb1 = str.substring(0, 1);
-                String numb2 = str.substring(1, 2);
                 sum += (number / 10) + (number % 10);
 
             }
