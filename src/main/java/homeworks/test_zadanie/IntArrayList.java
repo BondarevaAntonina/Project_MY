88c36671b140eea8375963a1d31a81423ba8cf7b
antoni <antoni@csltd.com.ua>
2018-05-11 17:25:18 +0300

    array

--- src/main/java/homeworks/test_zadanie/IntArrayList.java
+++ src/main/java/homeworks/test_zadanie/IntArrayList.java
@@ -1,19 +1,20 @@
 package homeworks.test_zadanie;
 
+import java.util.Arrays;
+
 /**
  * Создать имитацию коллекции(с помощью массива) ArrayList для работы с типом int.
  * Создать класс с полем типа массив.
  * Класс должен выполнять следующие операции:
- * 1) добавление элементов.
- * 2) изменение/удаление элементов по индексу.
- * 3) увеличение листа на заданное количество элементов.
+ * 1) добавление элементов. СДЕЛАНО
+ * 2) изменение/удаление элементов по индексу.  При удалении элемента не обнулять его, а удалять физически. ЧАСТИЧНО СДЕЛАНО
+ * 3) увеличение листа на заданное количество элементов. СДЕЛАНО
  * 4) уменьшение листа до заданного количество элементов.
  * 5) вывод элементов в консоль в прямом и обратном порядке.
  * 6) сортировка листа методом пузырька(http://study-java.ru/uroki-java/urok-11-sortirovka-massiva/).
  * 7) добавление массива в массив.
  * 8) удалять дубликаты.
  * 9) Поиск элемента методом линейного поиска.
- * При удалении элемента не обнулять его, а удалять физически.
  * Начальную размерность листа юзер вводит с консоли.
  * Создать меню для работы с листом из консоли.
  * Условие добавления: перезаписывать если элемент равен 0;
@@ -27,96 +28,114 @@ public class IntArrayList {
 
     //1 - Add an item to the list
     public static int[] addElementToEnd(int[] array, int element) {
-        //One option
-        /*
-        int arrayLength = myArray.length;  //+
-        System.out.println("Number of elements in the array myArray: " + arrayLength);
-        int[] myArray2 = new int[arrayLength + 1];
-        System.arraycopy( myArray, 0, myArray2, 0, myArray.length );
-        myArray2[myArray2.length - 1] = element;
-        toString(myArray2);
-*/
-        //Two option
+
         int[] myArray2 = new int[array.length + 1];
+
         for (int j = 0; j < array.length; j++) {
+
             myArray2[j] = array[j];
         }
 
         myArray2[myArray2.length - 1] = element;
-        //toString( myArray2);
+
         return myArray2;
     }
 
 
-//
-//    //2 - create method for resizing array
-//    public void removeElementByIndex(int index, int newElement) {
-//        array[index] = newElement;
-//    }
+    //2 - create method for resizing array
+    public static int[] removeElementByIndex(int[] array, int index, int value) {
+        array[index] = value;
+        return array;
+    }
 
     //3 - Delete items by index
-    public static int[] deleteElement(int[] array, int element) {
-        int[] myArray2 = new int[array.length - 1];
-        for (int j = 0; j < array.length; j++) {
-            myArray2[j] = array[j];
+    public static int[] deleteElementIndex(int[] array, int index) throws Exception {
+
+        if (index < 0) throw new Exception("Index can't < 0");
+
+        if (index > array.length - 1) throw new Exception("Index can't by > array size");
+
+        int[] new_array = new int[array.length - 1];
+
+        int j = 0;
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (i != index) new_array[j++] = array[i];
 
         }
-
-        myArray2[myArray2.length - 1] = element;
-        //toString( myArray2);
-        return myArray2;
+        return new_array;
     }
-
-//    //3 - Delete items by index
-//    public void deleteElementIndex(int index, int newElement) {
-//        array[index] = newElement;
-//        if (index >= 0 && index < array.length) {
-//            int[] copy = new int[array.length - 1];
-//            System.out.println();
-//        }
-//        return;
-//    }
-
 
 
     //4 - Increase the sheet by a specified number of elements
-    public void increaseElement() {
+    public static int[] addEmptyElement(int[] array, int addcount) throws Exception {
 
+        //new array with values already added
+        int[] tmparray = new int[array.length + addcount];
+
+        //copy the new array values from the old one
+        System.arraycopy(array, 0, tmparray, 0, array.length);
+
+        return tmparray;
     }
 
+
     //5 - Reducing the sheet to a specified number of elements
-    public void decreasingElement() {
+    public static int[] deleteEmptyElement(int[] array, int colelement) throws Exception {
+
+        if (colelement > array.length) throw new Exception("The number of elements removed is greater than the array");
+/*
+        int[] new_array = new int[array.length - colelement];
+        for (int i = 0; i < array.length - colelement ; i++) {
+            new_array[i] = array[i];
+        }
+*/
+        return Arrays.copyOf(array, array.length - colelement);
 
     }
 
     //6 - Output elements to the console in the forward and reverse order
-    public void forwardReverseOrder() {
+    public static int[] forwardReverseOrder(int[] array) {
 
+        for (int j = 0; j < array.length; j++) {
+            System.out.println(array[j]);
+        }
+        return array;
     }
 
-    //7 - Sorting sheet by bubble method
+    //7 - Output of elements to the console in the opposite direction
+    public static int[] reverseOrder(int[] array) {
+
+        for (int j = array.length - 1; j >= 0; j--) {
+            System.out.println(array[j]);
+        }
+        return array;
+    }
+
+    //8 - Sorting sheet by bubble method
     public void blisterSort() {
 
     }
 
-    //8 - Adding an Array to an Array
+    //9 - Adding an Array to an Array
     public void addArrayToArray() {
 
     }
 
-    //9 - Delete duplicates
+    //10 - Delete duplicates
     public void deleteDuplicat() {
 
     }
 
-    //10 - Searching for an element using the linear search method
+    //11 - Searching for an element using the linear search method
     public void searchLinear() {
 
     }
 
     public static void toString(int[] val) {
         for (int j = 0; j < val.length; j++) {
-            System.out.println(val[j]);
+            System.out.print(" " + val[j]);
         }
     }
 }
--- src/main/java/homeworks/test_zadanie/OperationArraList.java
+++ src/main/java/homeworks/test_zadanie/OperationArraList.java
@@ -8,11 +8,42 @@ public class OperationArraList {
     public static void main(String[] args) throws Exception {
 
         int[] account = new int[]{5, 4, 7, 8, 3, 2, 9, 4, 7, 5};
-        int[] newarray= IntArrayList.addElementToEnd(account, 44);
-        IntArrayList.toString( newarray);
-        System.out.println();
 
-        int[] arraydelete=IntArrayList.deleteElement(account,9);
-        IntArrayList.toString( arraydelete);
+
+        //1 - Add an item to the list
+        int[] newarray = IntArrayList.addElementToEnd(account, 44);
+        IntArrayList.toString(newarray);
+        System.out.println("1 - Add an item to the list");
+
+        //2 - create method for resizing array
+        int[] removearray = IntArrayList.removeElementByIndex(account, 5, 4);
+        IntArrayList.toString(removearray);
+        System.out.println("2 - create method for resizing array");
+
+        //3 - Delete items by index
+        int[] arraydelete = IntArrayList.deleteElementIndex(account, 9);
+        IntArrayList.toString(arraydelete);
+        System.out.println("3 - Delete items by index");
+
+        //4 - Increase the sheet by a specified number of elements
+        int[] addempty = IntArrayList.addEmptyElement(account,5);
+        IntArrayList.toString(addempty);
+        System.out.println("4 - Increase the sheet by a specified number of elements");
+
+        //5 - Reducing the sheet to a specified number of elements
+        int[] deleteempty = IntArrayList.deleteEmptyElement(account,2);
+        IntArrayList.toString(deleteempty);
+        System.out.println("5 - Reducing the sheet to a specified number of elements");
+
+        //6 - Output elements to the console in the forward and reverse order
+        int[] forward = IntArrayList.forwardReverseOrder(account);
+        IntArrayList.toString(forward);
+        System.out.println("6 - Output elements to the console in the forward and reverse order");
+
+        //7 - Output of elements to the console in the opposite direction
+        int[] reverse = IntArrayList.reverseOrder(account);
+        IntArrayList.toString(reverse);
+        System.out.println("7 - Output of elements to the console in the opposite direction");
+
     }
 }
