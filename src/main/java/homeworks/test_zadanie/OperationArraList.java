ff8aed98d3d0d67403dc8cadb4519b4b25a29886
antoni <antoni@csltd.com.ua>
2018-05-11 12:08:27 +0300

    array

--- src/main/java/homeworks/test_zadanie/OperationArraList.java
+++ src/main/java/homeworks/test_zadanie/OperationArraList.java
@@ -5,11 +5,12 @@ package homeworks.test_zadanie;
  */
 public class OperationArraList {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         int[] account = new int[]{5, 4, 7, 8, 3, 2, 9, 4, 7, 5};
-        int[] newarray= IntArrayList.addElementToEnd(account, 77);
+        int[] newarray= IntArrayList.addElementToEnd(account, 44);
         IntArrayList.toString( newarray);
+        System.out.println();
 
         int[] arraydelete=IntArrayList.deleteElement(account,9);
         IntArrayList.toString( arraydelete);
