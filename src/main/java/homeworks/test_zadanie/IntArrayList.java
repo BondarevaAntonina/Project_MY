1363aeb80c38e1cc699a16be80fbd210faff3faa
antoni <antoni@csltd.com.ua>
2018-05-16 15:25:28 +0300

    array

--- src/main/java/homeworks/test_zadanie/IntArrayList.java
+++ src/main/java/homeworks/test_zadanie/IntArrayList.java
@@ -7,15 +7,15 @@ import java.util.Arrays;
  * Создать класс с полем типа массив.
  * Класс должен выполнять следующие операции:
  * 1) добавление элементов. СДЕЛАНО
- * 2) изменение/удаление элементов по индексу.  При удалении элемента не обнулять его, а удалять физически. ЧАСТИЧНО СДЕЛАНО
+ * 2) изменение/удаление элементов по индексу.  При удалении элемента не обнулять его, а удалять физически. СДЕЛАНО
  * 3) увеличение листа на заданное количество элементов. СДЕЛАНО
- * 4) уменьшение листа до заданного количество элементов.
- * 5) вывод элементов в консоль в прямом и обратном порядке.
- * 6) сортировка листа методом пузырька(http://study-java.ru/uroki-java/urok-11-sortirovka-massiva/).
- * 7) добавление массива в массив.
- * 8) удалять дубликаты.
+ * 4) уменьшение листа до заданного количество элементов. СДЕЛАНО
+ * 5) вывод элементов в консоль в прямом и обратном порядке. СДЕЛАНО
+ * 6) сортировка листа методом пузырька(http://study-java.ru/uroki-java/urok-11-sortirovka-massiva/). СДЕЛАНО
+ * 7) добавление массива в массив. СДЕЛАНО
+ * 8) удалять дубликаты. СДЕЛАНО
  * 9) Поиск элемента методом линейного поиска.
- * Начальную размерность листа юзер вводит с консоли.
+ * Начальную размерность листа юзер вводит с консоли. СДЕЛАНО
  * Создать меню для работы с листом из консоли.
  * Условие добавления: перезаписывать если элемент равен 0;
  * Все действия юзера записывать в файл с название Logs.txt в формате дата + название метода + входящие параметры.
@@ -27,13 +27,13 @@ public class IntArrayList {
 
 
     //1 - Add an item to the list
-    public static int[] addElementToEnd(int[] array, int element) {
+    public static int[] addElementToEnd(int[] arraylist, int element) {
 
-        int[] myArray2 = new int[array.length + 1];
+        int[] myArray2 = new int[arraylist.length + 1];
 
-        for (int j = 0; j < array.length; j++) {
+        for (int j = 0; j < arraylist.length; j++) {
 
-            myArray2[j] = array[j];
+            myArray2[j] = arraylist[j];
         }
 
         myArray2[myArray2.length - 1] = element;
@@ -43,25 +43,25 @@ public class IntArrayList {
 
 
     //2 - create method for resizing array
-    public static int[] removeElementByIndex(int[] array, int index, int value) {
-        array[index] = value;
-        return array;
+    public static int[] removeElementByIndex(int[] arraylist, int index, int value) {
+        arraylist[index] = value;
+        return arraylist;
     }
 
     //3 - Delete items by index
-    public static int[] deleteElementIndex(int[] array, int index) throws Exception {
+    public static int[] deleteElementIndex(int[] arraylist, int index) throws Exception {
 
         if (index < 0) throw new Exception("Index can't < 0");
 
-        if (index > array.length - 1) throw new Exception("Index can't by > array size");
+        if (index > arraylist.length - 1) throw new Exception("Index can't by > array size");
 
-        int[] new_array = new int[array.length - 1];
+        int[] new_array = new int[arraylist.length - 1];
 
         int j = 0;
 
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < arraylist.length; i++) {
 
-            if (i != index) new_array[j++] = array[i];
+            if (i != index) new_array[j++] = arraylist[i];
 
         }
         return new_array;
@@ -69,53 +69,62 @@ public class IntArrayList {
 
 
     //4 - Increase the sheet by a specified number of elements
-    public static int[] addEmptyElement(int[] array, int addcount) throws Exception {
+    public static int[] addEmptyElement(int[] arraylist, int addcount) throws Exception {
 
         //new array with values already added
-        int[] tmparray = new int[array.length + addcount];
+        int[] tmparray = new int[arraylist.length + addcount];
 
         //copy the new array values from the old one
-        System.arraycopy(array, 0, tmparray, 0, array.length);
+        System.arraycopy(arraylist, 0, tmparray, 0, arraylist.length);
 
         return tmparray;
     }
 
 
     //5 - Reducing the sheet to a specified number of elements
-    public static int[] deleteEmptyElement(int[] array, int colelement) throws Exception {
+    public static int[] deleteEmptyElement(int[] arraylist, int colelement) throws Exception {
 
-        if (colelement > array.length) throw new Exception("The number of elements removed is greater than the array");
+        if (colelement > arraylist.length) throw new Exception("The number of elements removed is greater than the array");
 /*
         int[] new_array = new int[array.length - colelement];
         for (int i = 0; i < array.length - colelement ; i++) {
             new_array[i] = array[i];
         }
 */
-        return Arrays.copyOf(array, array.length - colelement);
+        return Arrays.copyOf(arraylist, arraylist.length - colelement);
 
     }
 
     //6 - Output elements to the console in the forward and reverse order
-    public static int[] forwardReverseOrder(int[] array) {
+    public static int[] forwardReverseOrder(int[] arraylist) {
 
-        for (int j = 0; j < array.length; j++) {
-            System.out.println(array[j]);
+        for (int j = 0; j < arraylist.length; j++) {
+            System.out.println(arraylist[j]);
         }
-        return array;
+        return arraylist;
     }
 
     //7 - Output of elements to the console in the opposite direction
-    public static int[] reverseOrder(int[] array) {
+    public static int[] reverseOrder(int[] arraylist) {
 
-        for (int j = array.length - 1; j >= 0; j--) {
-            System.out.println(array[j]);
+        for (int j = arraylist.length - 1; j >= 0; j--) {
+            System.out.println(arraylist[j]);
         }
-        return array;
+        return arraylist;
     }
 
     //8 - Sorting sheet by bubble method
-    public void blisterSort() {
-
+    public static int[] blisterSort(int[] arraylist) {
+        for (int i = arraylist.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arraylist[j] > arraylist[j + 1]) {
+                    int tmp = arraylist[j];
+                    arraylist[j] = arraylist[j + 1];
+                    arraylist[j + 1] = tmp;
+                }
+            }
+        }
+        return arraylist;
     }
 
     //9 - Adding an Array to an Array
@@ -124,14 +133,45 @@ public class IntArrayList {
     }
 
     //10 - Delete duplicates
-    public void deleteDuplicat() {
+    public static int[] deleteDublicat(int[] arraylist, int value) {
 
+        int dub_count = 0;
+        for (int j = 0; j < arraylist.length; j++) {
+            if (arraylist[j] == value) dub_count++;
+        }
+
+        if (dub_count > 1) {
+            int[] tmp_array = new int[arraylist.length - dub_count];
+            int t = 0;
+
+            for (int j = 0; j < arraylist.length; j++) {
+                if (arraylist[j] != value) {
+                    tmp_array[t] = arraylist[j];
+                    t++;
+                }
+            }
+            return tmp_array;
+        } else {
+            return arraylist;
+        }
     }
 
     //11 - Searching for an element using the linear search method
-    public void searchLinear() {
+    public static int[] searchLinear(int[] arraylist, int item) {
+        int d;
 
+            for (d = 0; d < arraylist.length; d++) {
+                if (arraylist[d] == item) {
+                    System.out.println(item + " present oneself " + (d + 1) + " on account in an array");
+                    break;
+                }
+            }
+        if (d == arraylist.length) {
+            System.out.println("Number " + item + " not found in array");
+        }
+        return arraylist;
     }
+
 
     public static void toString(int[] val) {
         for (int j = 0; j < val.length; j++) {
--- src/main/java/homeworks/test_zadanie/OperationArraList.java
+++ src/main/java/homeworks/test_zadanie/OperationArraList.java
@@ -1,5 +1,7 @@
 package homeworks.test_zadanie;
 
+import java.util.Scanner;
+
 /**
  * Created by antoni on 10.05.2018.
  */
@@ -7,43 +9,82 @@ public class OperationArraList {
 
     public static void main(String[] args) throws Exception {
 
-        int[] account = new int[]{5, 4, 7, 8, 3, 2, 9, 4, 7, 5};
+        int counter;
+        int arraylist[];
+        int numm;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        numm = input.nextInt();
+        arraylist = new int[numm];
+
+        System.out.println("Enter the " + numm + " numbers");
+        for (counter = 0; counter < numm; counter++) {
+            arraylist[counter] = input.nextInt();
+        }
+
+
 
 
         //1 - Add an item to the list
-        int[] newarray = IntArrayList.addElementToEnd(account, 44);
+        int[] newarray = IntArrayList.addElementToEnd(arraylist, 77);
+        System.out.println("Add an item to the list");
         IntArrayList.toString(newarray);
-        System.out.println("1 - Add an item to the list");
+
 
         //2 - create method for resizing array
-        int[] removearray = IntArrayList.removeElementByIndex(account, 5, 4);
+        int[] removearray = IntArrayList.removeElementByIndex(arraylist, 5, 4);
+        System.out.println(" 2 - create method for resizing array");
         IntArrayList.toString(removearray);
-        System.out.println("2 - create method for resizing array");
+
 
         //3 - Delete items by index
-        int[] arraydelete = IntArrayList.deleteElementIndex(account, 9);
+        int[] arraydelete = IntArrayList.deleteElementIndex(arraylist, 9);
+        System.out.println(" 3 - Delete items by index");
         IntArrayList.toString(arraydelete);
-        System.out.println("3 - Delete items by index");
+
 
         //4 - Increase the sheet by a specified number of elements
-        int[] addempty = IntArrayList.addEmptyElement(account,5);
+        int[] addempty = IntArrayList.addEmptyElement(arraylist,5);
+        System.out.println(" 4 - Increase the sheet by a specified number of elements");
         IntArrayList.toString(addempty);
-        System.out.println("4 - Increase the sheet by a specified number of elements");
+
 
         //5 - Reducing the sheet to a specified number of elements
-        int[] deleteempty = IntArrayList.deleteEmptyElement(account,2);
+        int[] deleteempty = IntArrayList.deleteEmptyElement(arraylist,2);
+        System.out.println(" 5 - Reducing the sheet to a specified number of elements");
         IntArrayList.toString(deleteempty);
-        System.out.println("5 - Reducing the sheet to a specified number of elements");
+
 
         //6 - Output elements to the console in the forward and reverse order
-        int[] forward = IntArrayList.forwardReverseOrder(account);
+        int[] forward = IntArrayList.forwardReverseOrder(arraylist);
+        System.out.println(" 6 - Output elements to the console in the forward and reverse order");
         IntArrayList.toString(forward);
-        System.out.println("6 - Output elements to the console in the forward and reverse order");
+
 
         //7 - Output of elements to the console in the opposite direction
-        int[] reverse = IntArrayList.reverseOrder(account);
+        int[] reverse = IntArrayList.reverseOrder(arraylist);
+        System.out.println(" 7 - Output of elements to the console in the opposite direction");
         IntArrayList.toString(reverse);
-        System.out.println("7 - Output of elements to the console in the opposite direction");
+
+
+        //8 - Sorting sheet by bubble method
+        int[] blister = IntArrayList.blisterSort(arraylist);
+        System.out.println(" 8 - Sorting sheet by bubble method");
+        IntArrayList.toString(blister);
+
+
+        //10 - Delete duplicates
+        int[] dublicat = IntArrayList.deleteDublicat(arraylist, 5);
+        System.out.println(" 10 - Delete duplicates");
+        IntArrayList.toString(dublicat);
+
+
+        //11 - Searching for an element using the linear search method
+        int[] search = IntArrayList.searchLinear(arraylist, 2);
+        System.out.println(" 11 - Searching for an element using the linear search method");
+        IntArrayList.toString(search);
+
 
     }
 }
