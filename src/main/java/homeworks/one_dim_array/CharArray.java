67f72a1ce347677af930f2b952a95e08271dab83
antoni <antoni@csltd.com.ua>
2018-05-23 13:37:57 +0300

    CharArray

--- src/main/java/homeworks/one_dim_array/CharArray.java
+++ src/main/java/homeworks/one_dim_array/CharArray.java
@@ -6,11 +6,17 @@ package homeworks.one_dim_array;
  */
 
 public class CharArray {
+
     public static void main(String[] args) {
+
         String str = "Перевернуть физически строку наоборот(отсортировать)";
+
         char[] reverseArray = str.toCharArray();
+
         System.out.print("The array in reverse order: ");
+
         System.out.println();
+
         for (int i = reverseArray.length - 1; i >= 0; i--) System.out.print(reverseArray[i]);
 
     }
