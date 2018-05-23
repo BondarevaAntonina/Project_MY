966e19bc054c563bfbbc0b59299cb22961a6984f
antoni <antoni@csltd.com.ua>
2018-05-23 16:46:39 +0300

    Sentence

---
+++ src/main/java/homeworks/one_dim_array/MassZero.java
@@ -0,0 +1,19 @@
+package homeworks.one_dim_array;
+
+import java.util.Scanner;
+
+/**
+ * Created by antoni on 23.05.2018.
+ */
+public class MassZero {
+    public static void main(String[] args) throws Exception {
+        int[] array;
+        array = new int[10];
+        int maxValue = 100;
+        int counter;
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+
+    }
+}
+
