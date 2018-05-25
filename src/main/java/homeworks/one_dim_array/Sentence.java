f6a4ea99e75e2206678382b9821886754a394626
antoni <antoni@csltd.com.ua>
2018-05-25 17:25:35 +0300

    Sentence

--- src/main/java/homeworks/one_dim_array/Sentence.java
+++ src/main/java/homeworks/one_dim_array/Sentence.java
@@ -2,40 +2,25 @@ package homeworks.one_dim_array;
 
 /**
  * Created by antoni on 17.05.2018.
- * В определенном предложении найти самое длинное и короткое слово.
+ * В определенном предложении найти самое длинное и короткое слово
  */
 public class Sentence {
-    public static void main(String[] args) {
-        String text = "The biggest and the smallest word must be somewhere here!";
-
-        String[] strings = text.split(" ");
-
-        String tmp = "";
-        String maxWord = "";
-        String minWord = text;
-
-        for (int i = 0; i < text.length(); i++) {
-
-            /*if (text.charAt(i) == ' ') {
-                maxWord = maxWord.length() < tmp.length() ? tmp : maxWord;
-                minWord = minWord.length() > tmp.length() ? tmp : minWord;
-                tmp = "";
-
-            } else {
-                tmp += text.charAt(i);
-            }*/
-
-            if (text.charAt(i) != ' ') {
-                tmp += text.charAt(i);
-                continue;
-            }
-
-            maxWord = maxWord.length() < tmp.length() ? tmp : maxWord;
-            minWord = minWord.length() > tmp.length() ? tmp : minWord;
-            tmp = "";
-
-        }
-
-        System.out.print("The biggest word is \"" + maxWord + "\" and the smallest one is \"" + minWord + "\"");
-    }
+//    public static void main(String[] args) {
+//        String text = "The biggest and the smallest word must be somewhere here!";
+//        String tmp = "";
+//        String maxWord = "";
+//        String minWord = text;
+//
+//        for (int i = 0; i <= text.length(); i++) {
+//
+//            if (i == text.length() || text.charAt(i) == ' ') {
+//                maxWord = maxWord.length() < tmp.length() ? tmp : maxWord;
+//                minWord = minWord.length() > tmp.length() ? tmp : minWord;
+//                tmp = "";
+//
+//            } else tmp += text.charAt(i);
+//        }
+//
+//        System.out.print("The biggest word is \"" + maxWord + "\" and the smallest one is \"" + minWord + "\"");
+//    }
 }
