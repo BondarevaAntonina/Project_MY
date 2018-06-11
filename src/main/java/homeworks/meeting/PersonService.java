7e23e35d255cbe654b7afbcd64699e75b19b5d9b
Тоня <panteratiger@ukr.net>
2018-06-11 11:20:06 +0300

    Log

--- src/main/java/homeworks/meeting/PersonService.java
+++ src/main/java/homeworks/meeting/PersonService.java
@@ -3,6 +3,7 @@ package homeworks.meeting;
 
 import homeworks.learn_enum.Sex;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class PersonService {
@@ -11,12 +12,12 @@ public class PersonService {
     public PersonService() {
         men = new Man[10];
     }
-
+//1
 //Регистрироваться человеку старше 18 лет
 
     public void addPersonWithMaxAge(Man man) {
 
-        if (man.getAge() <= 18) {
+        if (man.getAge() < 18) {
             System.out.println();
 
             return;
@@ -31,35 +32,37 @@ public class PersonService {
         }
 
         showSuitablePersonsByAgeAndSex(man);
-    }
 
+    }
+//2
     private void showSuitablePersonsByAgeAndSex(Man man) {
         for (Man temp : men) {
-            if (temp.getAge() == man.getAge() && temp.getSex() != man.getSex()) {
+            if (Objects.nonNull(temp) && temp.getAge() == man.getAge() && temp.getSex() != man.getSex()) {
                 man.showDataPerson();
             }
         }
     }
-
+//3
     // Просматривать зарегистрировавшихся людей. Для мужчин выводить только женщин и наоборот.
     public void showRegistrationPerson(Sex sex) {
         for (Man man : men) {
-            if (sex != man.getSex()) {
+            if (Objects.nonNull(man) && sex != man.getSex()) {
                 man.showDataPerson();
             }
         }
 
     }
-
+//4
     //Просматривать анкету отдельного человека(поиск по имени и фамилии)
     public void searchManByFirstNameAndLastName(String name, String surname) {
         for (Man man : men) {
-            if (man.getName().equals(name) && man.getSurname().equals(surname)) {
+            if (Objects.nonNull(man) && man.getName().equals(name) && man.getSurname().equals(surname)) {
                 man.showDataPerson();
             }
+            man.showDataPerson();
         }
     }
-
+//5
     //Организовать "умный поиск". Пользователь вводит требования(город, пол, возраст, количество детей)
     //   и  выводить людей, которые соответствуют требованиям
     public void smartSearchCitySexAgeChildren(String city, Sex sex, int age, int children) {
@@ -69,4 +72,24 @@ public class PersonService {
             }
         }
     }
+
+
+    public void resizeArray() {
+
+
+
+
+            if (Objects.isNull(men[men.length - 1])) {
+                int newSize = men.length * 2;
+
+                Man[] tmpArray = new Man[newSize];
+
+                System.arraycopy(men, 0, tmpArray, 0, men.length);
+
+                men = tmpArray;
+            }
+
+
+    }
+
 }
--- src/main/java/homeworks/meeting/TestMeeting.java
+++ src/main/java/homeworks/meeting/TestMeeting.java
@@ -31,10 +31,9 @@ public class TestMeeting {
             System.out.println("------- Dating for dating -------\n" +
                     "1 - To be registered to the person is more senior 18 years\n" +
                     "2 - After registration, list the suitable men / women for this person by age\n" +
-                    "3 - View people who have registered. For men, only women\n" +
-                    "4 - View people who have registered. For women, only men\n" +
-                    "5 - View the profile of an individual (search by name and surname)\n" +
-                    "6 - Organize \"smart search\". The user enters requirements (city, gender, age, number of children) and displays people who meet the requirements\n" +
+                    "3 - View people who have registered. For men, only women should be removed and vice versa\n" +
+                    "4 - View the profile of an individual (search by name and surname)\n" +
+                    "5 - Organize \"smart search\". The user enters requirements (city, gender, age, number of children) and displays people who meet the requirements\n" +
                     "0 - Exit");
             try {
                 userActionNumber = SCANNER.nextInt();
@@ -42,6 +41,7 @@ public class TestMeeting {
             } catch (Exception e) {
                 System.out.println("Input Error\n:  " + e);
                 userActionNumber = -1;
+                e.printStackTrace();
             }
         } while (userActionNumber != 0);
     }
@@ -76,23 +76,83 @@ public class TestMeeting {
 
                 System.out.print("Enter the city where you live:");
 
-                String city = sc.nextLine();
+                String city = sc.next();
 
                 Man man = new Man(sex, name, surname, age, children, city);
 
                 personService.addPersonWithMaxAge(man);
                 break;
             }
+            //После регистрации выводить список подходящих мужчин/ женщин для этого человека по возрасту
+            //2 - After registration, list the suitable men / women for this person by age
+            case 2: {
 
-            case 2:
+                System.out.print("Enter genter man");
 
-            System.out.print("Enter genter man");
+                Sex sex = Sex.valueOf(sc.nextLine().toUpperCase());
 
-            Sex sex = Sex.valueOf(sc.nextLine().toUpperCase());
+                personService.showRegistrationPerson(sex);
 
-            personService.showRegistrationPerson(sex);
+                break;
+            }
 
-            break;
+            //Просматривать зарегистрировавшихся людей. Для мужчин выводить только женщин и наоборот
+            // View people who have registered. For men, only women should be removed and vice versa
+            case 3: {
+
+                System.out.print("Enter genter man");
+
+                Sex sex = Sex.valueOf(sc.nextLine().toUpperCase());
+
+                personService.showRegistrationPerson(sex);
+
+                break;
+            }
+
+
+            //Просматривать анкету отдельного человека(поиск по имени и фамилии)
+            // View people who have registered. For men, only women should be removed and vice versa
+            case 4: {
+
+                System.out.println("Enter the person's name:");
+
+                String name = sc.nextLine();
+
+                System.out.println("Enter the person's surname:");
+
+                String surname = sc.nextLine();
+
+                personService.searchManByFirstNameAndLastName(name, surname);
+
+                break;
+            }
+
+            //Организовать "умный поиск". Пользователь вводит требования(город, пол, возраст, количество детей)
+            //   и  выводить людей, которые соответствуют требованиям
+            // Organize "smart search". The user enters requirements (city, gender, age, number of children) and displays people who meet the requirements
+            case 5: {
+
+                System.out.print("Enter the city where you live:");
+
+                String city = sc.nextLine();
+
+                System.out.print("Enter genter man");
+
+                Sex sex = Sex.valueOf(sc.nextLine().toUpperCase());
+
+                System.out.print("Enter the person's age:");
+
+                int age = SCANNER.nextInt();
+
+                System.out.print("Enter the number of children:");
+
+                int children = SCANNER.nextInt();
+
+                personService.smartSearchCitySexAgeChildren(city, sex, age, children);
+
+                break;
+            }
+
         }
     }
 }
