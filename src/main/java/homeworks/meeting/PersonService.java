f559c2530c8040aa8d41315487f2afa2851ec8d9
antoni <antoni@csltd.com.ua>
2018-06-12 11:57:18 +0300

    PersonService

--- src/main/java/homeworks/meeting/PersonService.java
+++ src/main/java/homeworks/meeting/PersonService.java
@@ -17,12 +17,13 @@ public class PersonService {
 
     public void addPersonWithMaxAge(Man man) {
 
-        if (man.getAge() < 18) {
+        resizeArray();
+
+        if (man.getAge() <= 18) {
             System.out.println();
 
             return;
         }
-
         for (int i = 0; i < men.length; i++) {
 
             if (Objects.isNull(men[i])) {
@@ -34,40 +35,53 @@ public class PersonService {
         showSuitablePersonsByAgeAndSex(man);
 
     }
-//2
+
+    //2
     private void showSuitablePersonsByAgeAndSex(Man man) {
         for (Man temp : men) {
+
             if (Objects.nonNull(temp) && temp.getAge() == man.getAge() && temp.getSex() != man.getSex()) {
+
                 man.showDataPerson();
             }
         }
     }
-//3
+
+    //3
     // Просматривать зарегистрировавшихся людей. Для мужчин выводить только женщин и наоборот.
     public void showRegistrationPerson(Sex sex) {
+
         for (Man man : men) {
+
             if (Objects.nonNull(man) && sex != man.getSex()) {
+
                 man.showDataPerson();
             }
         }
 
     }
-//4
+
+    //4
     //Просматривать анкету отдельного человека(поиск по имени и фамилии)
     public void searchManByFirstNameAndLastName(String name, String surname) {
         for (Man man : men) {
+
             if (Objects.nonNull(man) && man.getName().equals(name) && man.getSurname().equals(surname)) {
+
                 man.showDataPerson();
             }
             man.showDataPerson();
         }
     }
-//5
+
+    //5
     //Организовать "умный поиск". Пользователь вводит требования(город, пол, возраст, количество детей)
     //   и  выводить людей, которые соответствуют требованиям
     public void smartSearchCitySexAgeChildren(String city, Sex sex, int age, int children) {
         for (Man man : men) {
-            if (man.getCity().equals(city) && sex == man.getSex() && age==man.getAge() && children== man.getChildren()) {
+
+            if (Objects.nonNull(man) && man.getCity().equals(city) && sex == man.getSex() && age == man.getAge() && children == man.getChildren()) {
+
                 man.showDataPerson();
             }
         }
@@ -76,18 +90,16 @@ public class PersonService {
 
     public void resizeArray() {
 
+        if (Objects.isNull(men[men.length - 1])) {
 
+            int newSize = men.length * 2;
 
+            Man[] tmpArray = new Man[newSize];
 
-            if (Objects.isNull(men[men.length - 1])) {
-                int newSize = men.length * 2;
+            System.arraycopy(men, 0, tmpArray, 0, men.length);
 
-                Man[] tmpArray = new Man[newSize];
-
-                System.arraycopy(men, 0, tmpArray, 0, men.length);
-
-                men = tmpArray;
-            }
+            men = tmpArray;
+        }
 
 
     }
