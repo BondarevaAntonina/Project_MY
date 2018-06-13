d8517ec1e3e2bfb4a320714faf44e936c0ec4c9c
antoni <antoni@csltd.com.ua>
2018-06-13 11:00:12 +0300

    PersonService

--- src/main/java/lessons/work_with_files/WorkWithFiles.java
+++ src/main/java/lessons/work_with_files/WorkWithFiles.java
@@ -33,47 +33,4 @@ public class WorkWithFiles {
 //        List<String> strings = Files.readAllLines(path);
     }
 
-
-
-    private static void readUsingBufferedReader(String fileName, Charset cs) throws IOException {
-        File file = new File(fileName);
-        FileInputStream fis = new FileInputStream(file);
-        InputStreamReader isr = new InputStreamReader(fis, cs);
-        BufferedReader br = new BufferedReader(isr);
-        String line;
-        while((line = br.readLine()) != null){
-            System.out.println(line);
-        }
-        br.close();
-
-    }
-
-    private static void readUsingBufferedReaderJava7(String fileName, Charset cs) throws IOException {
-        Path path = Paths.get(fileName);
-        BufferedReader br = Files.newBufferedReader(path, cs);
-        String line;
-        while((line = br.readLine()) != null){
-            System.out.println(line);
-        }
-        br.close();
-    }
-
-
-    private static void readUsingScanner(String fileName) throws IOException {
-        Path path = Paths.get(fileName);
-        Scanner scanner = new Scanner(path);
-        //читаем построчно
-        while(scanner.hasNextLine()){
-            String line = scanner.nextLine();
-        }
-    }
-
-    private static void readUsingFiles(String fileName) throws IOException {
-
-        Path path = Paths.get(fileName);
-        //считываем содержимое файла в массив байт
-        byte[] bytes = Files.readAllBytes(path);
-        //считываем содержимое файла в список строк
-        List<String> allLines = Files.readAllLines(path, StandardCharsets.UTF_8);
-    }
 }
