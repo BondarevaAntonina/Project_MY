410909a016569ca498554d4c7d1437353f4758db
antoni <antoni@csltd.com.ua>
2018-06-18 15:20:27 +0300

    PersonService

---
+++ src/main/java/homeworks/regular_expressions/AmountNumberDigitThree.java
@@ -0,0 +1,12 @@
+package homeworks.regular_expressions;
+
+/**
+ * Пользователь вводит имя и фамилию. Буквы могут быть в разных регистрах.
+ * Проверить, чтобы первые буквы были в верхнем регистре, а остальные буквы - в нижнем.
+ */
+public class AmountNumberDigitThree {
+
+    public static void main(String[] args) {
+
+    }
+}
