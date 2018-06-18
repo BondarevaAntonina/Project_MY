1cb6ac126f6361f0bf58a974dcb9e07ad8d501f1
antoni <antoni@csltd.com.ua>
2018-06-18 10:57:27 +0300

    PersonService

--- src/main/java/homeworks/regular_expressions/AmountNumberDigit.java
+++ src/main/java/homeworks/regular_expressions/AmountNumberDigit.java
@@ -1,15 +1,28 @@
 package homeworks.regular_expressions;
 
+import java.util.regex.Pattern;
+
 /**
  * Любое количество букв, а потом две цифры и наоборот
  */
 
-import java.util.regex.Pattern;
-
 public class AmountNumberDigit {
+
+//    public static void main(String[] args) {
+//
+//        String regex = "Any number of letters (\\d{2}) and then two digits";
+//
+//        String text = "Any number of letters 36 and then two digits";
+//
+//        System.out.println(Pattern.matches(regex, text));
+//    }
+
+
     public static void main(String[] args) {
-        String regex = "Any number of letters (\\d{2}) and then two digits";
-        String text = "Any number of letters 36 and then two digits";
+
+        String regex = "232 234234 234 234234 \\w{2} 345 45345 345 25445";
+
+        String text = "232 234234 234 234234 is 345 45345 345 25445";
 
         System.out.println(Pattern.matches(regex, text));
     }
