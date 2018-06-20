6f6e963257ff7af2eac5de52c6ac728d7c4edbc5
antoni <antoni@csltd.com.ua>
2018-06-20 15:18:24 +0300

    Matcher

--- src/main/java/homeworks/regular_expressions/AmountNumberDigitFive.java
+++ src/main/java/homeworks/regular_expressions/AmountNumberDigitFive.java
@@ -1,5 +1,8 @@
 package homeworks.regular_expressions;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Пользователь вводит телефон в формате (ххх)ххх-хх-хх.
  (ххх) может быть таким: (095), (097), (073), (067), (099), (063).
@@ -10,6 +13,16 @@ package homeworks.regular_expressions;
  */
 public class AmountNumberDigitFive {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter the phone in the format (ххх)ххх-хх-хх");
+
+        String phone = sc.nextLine();
+
+        String[] splitq = phone.split("-\\d{3}-\\d{2}-\\d{2}");
+
+
+
+        System.out.println(Arrays.toString(splitq));
     }
 }
