0130d46079c2f60a85717fbb751e6ba95357e48c
antoni <antoni@csltd.com.ua>
2018-06-22 13:18:01 +0300

    Matcher

--- src/main/java/homeworks/regular_expressions/AmountNumberDigitFive.java
+++ src/main/java/homeworks/regular_expressions/AmountNumberDigitFive.java
@@ -1,28 +1,46 @@
 package homeworks.regular_expressions;
 
-import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Пользователь вводит телефон в формате (ххх)ххх-хх-хх.
- (ххх) может быть таким: (095), (097), (073), (067), (099), (063).
- Проверить, чтобы 7 - ый номер был в формате "(ххх)ххх-хх-хх".
- Если пользователь вводит номер начинающийся на(095) или (099) ххх-хх-хх, выводить в консоль "Пользователь имеет МТС номер",
- на(097) или (067) ххх-хх-хх, выводить в консоль "Пользователь имеет Киевстар номер",
- на(073) или (063) ххх-хх-хх, выводить в консоль "Пользователь имеет Лайф номер"
+ * (ххх) может быть таким: (095), (097), (073), (067), (099), (063).
+ * Проверить, чтобы 7 - ый номер был в формате "(ххх)ххх-хх-хх".
+ * Если пользователь вводит номер начинающийся на(095) или (099) ххх-хх-хх, выводить в консоль "Пользователь имеет МТС номер",
+ * на(097) или (067) ххх-хх-хх, выводить в консоль "Пользователь имеет Киевстар номер",
+ * на(073) или (063) ххх-хх-хх, выводить в консоль "Пользователь имеет Лайф номер"
  */
 public class AmountNumberDigitFive {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the phone in the format (ххх)ххх-хх-хх");
 
         String phone = sc.nextLine();
 
-        String[] splitq = phone.split("-\\d{3}-\\d{2}-\\d{2}");
+//      String phone = "(068)234-56-78";
 
-//        if(phone.equals())
+        Pattern p = Pattern.compile("\\(\\d{3}\\)");
 
-        System.out.println(Arrays.toString(splitq));
+        Matcher m = p.matcher(phone);
+
+        if (m.find()) {
+            if (m.group().equals("(095)") || m.group().equals("(099)")) {
+
+                System.out.println("User has MTS number");
+
+            } else if (m.group().equals("(068)") || m.group().equals("(067)")) {
+
+                System.out.println("The user has a Kyivstar number");
+
+            } else if (m.group().equals("(073)") || m.group().equals("(063)")) {
+
+                System.out.println("User has Life number");
+            }
+        }
     }
 }
