9d4695b65d6c0a2994f82d11aac105f255abd106
antoni <antoni@csltd.com.ua>
2018-07-02 17:03:51 +0300

    Matcher

--- src/main/java/homeworks/abstract_work/TestWriter.java
+++ src/main/java/homeworks/abstract_work/TestWriter.java
@@ -7,11 +7,16 @@ import java.util.Scanner;
  */
 public class TestWriter {
 
+    public TestWriter() {
+
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         String textUserConsole = sc.nextLine();
+
 
     }
 }
