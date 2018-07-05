e18b97d966cdcd1e60327101c3be115a1c61deea
antoni <antoni@csltd.com.ua>
2018-07-05 16:43:10 +0300

    Matcher

--- src/main/java/homeworks/overloading_overriding_home/Person.java
+++ src/main/java/homeworks/overloading_overriding_home/Person.java
@@ -2,32 +2,63 @@ package homeworks.overloading_overriding_home;
 
 /**
  * Создать класс Person с поля firstName (String), lastName (String), age (int), gender (String), phoneNumber (int),
- *  и пять перегруженных методов, которые задают эти поля с разными списками аргументов.
- *  Создайте класс, который будет использовать экземпляр класса Person и его методы.
+ * и пять перегруженных методов, которые задают эти поля с разными списками аргументов.
+ * Создайте класс, который будет использовать экземпляр класса Person и его методы.
  */
 public class Person {
-    String firstName;
-    String lastName;
-    int age;
-    String gender;
-    int phoneNumber;
 
-    public Person(String firstName, String lastName, int age, String gender, int phoneNumber) {
+    private String firstName = "Petrienko";
+    private String lastName = "Ivan";
+    private int age = 30;
+    private String gender = "M";
+    private int phoneNumber = 5252;
+
+    public Person() {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
         this.gender = gender;
         this.phoneNumber = phoneNumber;
+
     }
 
+
     public void print(String firstName, String lastName, int age, String gender, int phoneNumber) {
-        String dataPerson = firstName + " " + lastName + age + " " + gender + " " + phoneNumber;
+        String dataPerson = String.format("фамилия: %s имя: %s возраст: %d, пол: %s, телефон: %d", firstName, lastName, age, gender, phoneNumber);
         System.out.println(dataPerson);
+
     }
+
+    public void print(String firstName, String lastName, int age, String gender) {
+        String dataPerson2 = String.format("фамилия: %s имя: %s возраст: %d, пол: %s", firstName, lastName, age, gender);
+        System.out.println(dataPerson2);
+    }
+
+    public void print(String firstName, String lastName, int age) {
+        String dataPerson3 = String.format("фамилия: %s имя: %s возраст: %d", firstName, lastName, age);
+        System.out.println(dataPerson3);
+    }
+
+    public void print(String firstName, String lastName) {
+        String dataPerson4 = String.format("фамилия: %s имя: %s", firstName, lastName);
+        System.out.println(dataPerson4);
+    }
+
+    public void print(String firstName, String lastName, String gender) {
+        String dataPerson5 = String.format("фамилия: %s имя: %s пол: %s", firstName, lastName, gender);
+        System.out.println(dataPerson5);
+    }
+
 
     public static void main(String[] args) {
 
+        Person person = new Person();
 
+        person.print("Petrienko", "Ivan", 30, "M", 5252);
+        person.print("Poleev ", "Oleg", 35, "M ");
+        person.print("Sereda", "Pavel", 27, "20");
+        person.print("Chernishew", "Bogdan");
+        person.print("Cherkas", "Petya","M");
 
     }
 }
