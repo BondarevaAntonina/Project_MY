b5cc739fe40112e10bb9b5a78ac5b042dfbedb20
antoni <antoni@csltd.com.ua>
2018-07-05 14:25:00 +0300

    Matcher

---
+++ src/main/java/homeworks/overloading_overriding_home/Person.java
@@ -0,0 +1,14 @@
+package homeworks.overloading_overriding_home;
+
+/**
+ *   Создать класс Person с поля firstName (String), lastName (String), age (int), gender (String), phoneNumber (int),
+     и пять перегруженных методов, которые задают эти поля с разными списками аргументов.
+     Создайте класс, который будет использовать экземпляр класса Person и его методы.
+ */
+public class Person {
+    String firstName;
+    String lastName;
+    int age;
+    String gender;
+    int phoneNumber;
+}
