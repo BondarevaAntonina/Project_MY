90efdd6d90fef0a95f217e310111077526e61782
antoni <antoni@csltd.com.ua>
2018-07-09 16:00:26 +0300

    Files

--- src/main/java/homeworks/file_manager_application/FilesManager.java
+++ src/main/java/homeworks/file_manager_application/FilesManager.java
@@ -46,8 +46,6 @@ public class FilesManager {
 
             try {
 
-                // Creating The New Directory Structure
-
                 Files.createDirectories(dirPathObj);
 
                 System.out.println("New Directory Successfully Created");
