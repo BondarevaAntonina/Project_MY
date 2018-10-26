a1fffa8ad8375a372e545fdbd65b8e117739b7cb
Тоня <panteratiger@ukr.net>
2018-10-26 21:12:41 +0300

    Dictionary

--- src/main/java/homeworks/translator_application/Translator.java
+++ src/main/java/homeworks/translator_application/Translator.java
@@ -1,9 +1,13 @@
 package homeworks.translator_application;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 
 /**
@@ -25,14 +29,48 @@ public class Translator {
 
     private Map<Languages, Map<String, String>> map;
 
-    public static final Path PATH = Paths.get("./ForTranslator")$
+    public static final Path PATH = Paths.get("./ForTranslator");
 
-    public Translator() {
+    public Translator() throws IOException {
         this.map = new HashMap <>();
+
+        fillMap();
     }
 
-    public void fillMap() {
+    private void fillMap() throws IOException {
+        Files.newDirectoryStream(PATH).forEach(path -> {
+            String fileName = path.toFile().getName();//add validate to equals fileName with enum's object
 
+            String[] names = fileName.split("_"); //"rus"[0], "eng"[1]
+
+            String refersName = names[1].concat("_").concat(names[0]);
+            /*
+                * rus_eng -> eng_rus
+                * */
+            HashMap<String, String> right = new HashMap<>();
+
+            HashMap<String, String> refers = new HashMap<>();
+
+            try {
+
+                Files.readAllLines(path)
+                        .stream()
+                        .map(line -> line.split(":"))
+                        .forEach(words -> {//Hello:Привет
+
+                    right.put(words[0], words[1]);
+
+                    refers.put(words[1], words[0]);
+                });
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            map.put(Languages.valueOf(fileName), right);
+
+            map.put(Languages.valueOf(refersName), refers);
+        });
     }
 
 }
